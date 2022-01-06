2b09c69e388e61ad1779cd18e01fcde8111a6681
SMNPSStud <96935495+SMNPSStud@users.noreply.github.com>
2022-01-06 11:28:52 +0530

    Add files via upload

---
+++ ArinPareek.py
@@ -0,0 +1,27 @@
+import pygame
+pygame.init() 
+screen = pygame.display.set_mode((600, 600))
+
+#Create the box here
+box1=pygame.Rect(300,500,60,10)
+box2=pygame.Rect(200,500,60,10)
+box3=pygame.Rect(100,500,60,10)
+box4=pygame.Rect(120,500,60,10)
+box5=pygame.Rect(320,250,60,10)
+
+
+carryOn = True
+while carryOn:
+    for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                  carryOn = False           
+    screen.fill((36,90,190))
+    
+    pygame.draw.rect(screen,(255,192,203),box1)
+    pygame.draw.rect(screen,(255,192,203),box2)
+    pygame.draw.rect(screen,(255,192,203),box3)
+    pygame.draw.rect(screen,(255,192,203),box4)
+    pygame.draw.rect(screen,(255,192,203),box5)
+     
+    pygame.display.flip()
+pygame.quit()
