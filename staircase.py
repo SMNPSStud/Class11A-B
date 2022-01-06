c66ec8094ce8e84dc63444a398054385d9a793c1
SMNPSStud <96935495+SMNPSStud@users.noreply.github.com>
2022-01-06 11:15:28 +0530

    Add files via upload

---
+++ staircase.py
@@ -0,0 +1,30 @@
+import pygame
+pygame.init() 
+screen = pygame.display.set_mode((600,600))
+
+box1=pygame.Rect(300,500,60,10)
+box2=pygame.Rect(250,450,60,10)
+box3=pygame.Rect(200,400,60,10)
+box4=pygame.Rect(150,350,60,10)
+box5=pygame.Rect(100,300,60,10)
+box6=pygame.Rect(50,250,60,10)
+carryOn = True
+while carryOn:
+    for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                  carryOn = False           
+    screen.fill((36,90,190))
+    
+    pygame.draw.rect(screen,(255,192,203),box1)
+    pygame.draw.rect(screen,(255,192,203),box2)
+    pygame.draw.rect(screen,(255,192,203),box3)
+    pygame.draw.rect(screen,(255,192,203),box4)
+    pygame.draw.rect(screen,(255,192,203),box5)
+    pygame.draw.rect(screen,(255,192,203),box6)
+    
+
+
+
+    pygame.display.flip()
+pygame.quit()
+    
